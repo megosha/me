1d0c9bd3a631e0a147e9a21c2af516692767240a
Margarita <mv@asofts.ru>
2020-07-10 16:23:48 +0700

    switch index.html theme with cookie saving

--- cssjs/switch_theme.js
+++ cssjs/switch_theme.js
@@ -1,21 +1,15 @@
 var btn = document.getElementById("theme-button");
 var link = document.getElementById("theme-link");
 
-// btn.addEventListener("click", function () { ChangeTheme(); });
 
 function ChangeTheme(btn_id)
 {
-    // let lightTheme = "cssjs/light.css";
-    // let darkTheme = "cssjs/dark.css";
-    let path_theme = `cssjs/${btn_id}.css`;
-
     var currTheme = link.getAttribute("href");
-    // var theme = "";
 
-   	 currTheme = path_theme;
-   	 // theme = "light";
-
-    link.setAttribute("href", currTheme);
-
-    // Save(theme);
+    let path_theme = `cssjs/${btn_id}.css`;
+    document.cookie = `theme-choice=${btn_id}`;
+    if (currTheme !== path_theme)
+        link.setAttribute("href", path_theme);
+    else
+        alert(':P');
 }
