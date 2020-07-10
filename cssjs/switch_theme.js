31f92d087d077adbf99916767b64df68b25e287e
Margarita <mv@asofts.ru>
2020-07-10 13:53:06 +0700

    switch index.html theme without cookie saving

--- cssjs/switch_theme.js
+++ cssjs/switch_theme.js
@@ -1,26 +1,19 @@
 var btn = document.getElementById("theme-button");
 var link = document.getElementById("theme-link");
 
-btn.addEventListener("click", function () { ChangeTheme(); });
+// btn.addEventListener("click", function () { ChangeTheme(); });
 
-function ChangeTheme()
+function ChangeTheme(btn_id)
 {
-    let lightTheme = "cssjs/light.css";
-    let darkTheme = "cssjs/dark.css";
+    // let lightTheme = "cssjs/light.css";
+    // let darkTheme = "cssjs/dark.css";
+    let path_theme = `cssjs/${btn_id}.css`;
 
     var currTheme = link.getAttribute("href");
-    var theme = "";
+    // var theme = "";
 
-    if(currTheme === lightTheme)
-    {
-   	 currTheme = darkTheme;
-   	 // theme = "dark";
-    }
-    else
-    {
-   	 currTheme = lightTheme;
+   	 currTheme = path_theme;
    	 // theme = "light";
-    }
 
     link.setAttribute("href", currTheme);
 
