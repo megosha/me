7ec8187face2d3b158e0d5b7c233491a91613b0e
Margarita <mv@asofts.ru>
2020-07-16 18:55:53 +0700

    portfolio.html hide ando show description without animation

--- cssjs/portfolio.js
+++ cssjs/portfolio.js
@@ -3,10 +3,29 @@ var works = {
     "1": {"title": "qwe2", "description": "qweqwe", "price": "123", "images": ["3-610x308.png", "03-1.jpg"]},
     "2": {"title": "qwe3", "description": "qweqwe", "price": "123", "images": ["-966x491.png", "05.jpg"]},
     "3": {"title": "qwe4", "description": "qweqwe", "price": "123", "images": ["dezinsector.jpg", "03-1.jpg"]},
-    "4": {"title": "qwe5", "description": "qweqwe", "price": "123", "images": ["-966x485.png", "08.jpg"]},
+    "4": {"title": "qwe5", "description": "qweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqwe qweqweqweqweqweqwe qweqweqweqwe  qweqweqweqwe   qweqwe", "price": "123", "images": ["-966x485.png", "08.jpg"]},
 };
 
-function carousel_form(title, description, price, images) {
+
+function all_works(return_id) {
+    var descr = document.getElementById('description');
+    descr.innerText = '';
+    descr.setAttribute('hidden', true);
+     // $(descr).fadeOut("slow");
+    for (key in Object.keys(works)){
+            if (return_id !== key){
+                work_preview = document.getElementById(Object.keys(works)[key]);
+                work_preview.removeAttribute('hidden');
+            }
+        }
+    document.getElementById(return_id).scrollIntoView({behavior: 'smooth', block: 'center'});
+    section = document.getElementById('extGallery6-1f');
+    section.setAttribute('hidden', true);
+
+}
+
+
+function carousel_form(work_id, title, description, price, images) {
     var li = '';
     var items = '';
     for (img in images){
@@ -28,19 +47,23 @@ function carousel_form(title, description, price, images) {
 <div class="row justify-content-center">
 
 
-            <div class="col-md-6">
-                <div class="text-box align-left">
-                    <h4 class="card-title1 mbr-fonts-style mbr-normal display-5"><br>${title}</h4>
-                    <div class="ico-box">
-                        <span class="pr-2 mbr-iconfont mbr-iconfont-social ico2 mbri-pin"></span>
-                        <p class="mbr-text mbr-fonts-style item mbr-normal display-7">${description}</p>
-                    </div>
-                </div>
-            </div>
+<!--            <div class="col-md-6">-->
+<!--                <div class="text-box align-left">-->
+<!--                    <h4 class="card-title1 mbr-fonts-style mbr-normal display-5"><br>${title}</h4>-->
+<!--                    <div class="ico-box">-->
+<!--                        <span class="pr-2 mbr-iconfont mbr-iconfont-social ico2 mbri-pin"></span>-->
+<!--                        <p class="mbr-text mbr-fonts-style item mbr-normal display-7">${description}</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
             <div class="col-md-6">
                 <div class="text-box align-right">
-                    <h4 class="card-title2 mbr-fonts-style mbr-normal display-5">${price}</h4>
-<!--                    <p class="mbr-text mbr-fonts-style sq mbr-normal display-7">$777 / sq m</p>-->
+<!--                    <h4 class="card-title2 mbr-fonts-style mbr-normal display-5">Другие работы</h4>-->
+                     <button class="btn btn-primary btn-sm" type="button" onclick="all_works(${work_id})">Другие работы</button>
+<!--                    <p class="mbr-text mbr-fonts-style sq mbr-normal display-7">${price}</p>-->
+                    <div class="card__wrap col-lg-4 col-md-6 col-sm-6">
+                
+            </div>
                 </div>
             </div>
                                         
@@ -66,29 +89,32 @@ function carousel_form(title, description, price, images) {
 }
 
 function portfolio_item(number) {
-    section = document.getElementById('extGallery6-1f');
+    var section = document.getElementById('extGallery6-1f');
+    var descr = document.getElementById('description');
     var work = works[number];
     if (work !== undefined) {
         var title = work['title'];
         var description = work['description'];
         var price = work['price'];
         var images = work['images'];
-        section.innerHTML = carousel_form(title, description, price, images);
+        section.innerHTML = carousel_form(number, title, description, price, images);
 
-        // for (key in Object.keys(works)){
-        //     if (number !== key){
-        //         work_preview = document.getElementById(Object.keys(works)[key]);
-        //         work_preview.setAttribute('hidden', true);
-        //     }
-        // }
+        for (key in Object.keys(works)){
+            if (number !== key){
+                work_preview = document.getElementById(Object.keys(works)[key]);
+                work_preview.setAttribute('hidden', true);
+            }
+        }
 
 
         section.removeAttribute('hidden');
+        descr.innerText = description;
+        descr.removeAttribute('hidden');
         window.initTestimonialsPlugin = true;
         $('.testimonials-slider').each(function () {
             initTestimonialsCarousel(this);
         });
-        section.scrollIntoView({behavior: 'smooth', block: 'center'});
+        // section.scrollIntoView({behavior: 'smooth', block: 'center'});
     }
     else return false;
 
