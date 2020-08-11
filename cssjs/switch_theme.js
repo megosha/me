60aa696193e5407e2ce6fb31e819e4158b476f49
Margarita <mv@asofts.ru>
2020-08-11 18:27:17 +0700

    shake page while switching theme

--- cssjs/switch_theme.js
+++ cssjs/switch_theme.js
@@ -1,15 +1,27 @@
 var btn = document.getElementById("theme-button");
 var link = document.getElementById("theme-link");
-
+var body = document.getElementById("body");
 
 function ChangeTheme(btn_id)
 {
+
     var currTheme = link.getAttribute("href");
 
     let path_theme = `cssjs/${btn_id}.css`;
     document.cookie = `theme-choice=${btn_id}`;
-    if (currTheme !== path_theme)
+    if (currTheme !== path_theme){
         link.setAttribute("href", path_theme);
+        // body.classList.remove('wibro');
+    }
     else
-        alert(':P');
+        // {
+    //     if (body.classList.contains('wibro')){
+    //         body.classList.remove('wibro');
+    //     }
+    //     body.classList.add('wibro');
+    // }
+    body.classList.add('wibro');
+    setTimeout(() => body.classList.remove('wibro'), 250);
+
+        // alert(':P');
 }
